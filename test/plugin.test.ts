0b7a0d061cbedba5bc323f709336ab6ab782c83f
Eduardo Davalos <davalosaeduardo@gmail.com>
2023-01-25 13:48:04 -0600

    chore(testing setup): Added jest for testing the package.

---
+++ test/plugin.test.ts
@@ -0,0 +1,14 @@
+// import mitt, { Emitter } from 'mitt'
+// import { ChimeraJSIntegrator } from '../src/ChimeraJSIntegrator'
+
+// describe('installing plugin should broadcast event bus', () => {
+
+    // Setup
+    // beforeEach(() => {
+
+    // })
+// })
+
+test('asdf', () => {
+    expect(1).toBe(1)
+})
